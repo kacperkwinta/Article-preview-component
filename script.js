e877c884acc94b2e3f53649f97132e56cf9ef8f7
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-01-23 12:32:02 +0100

    done! ✨

--- script.js
+++ script.js
@@ -2,13 +2,26 @@
 
 console.log("hi mom");
 
-// selecting elements
+// popup desktop
 const popupWindow = document.querySelector(".popup-window");
+// popup mobile
+const popupMobileWindow = document.querySelector(".popup-window-mobile");
+// container with profile info
+const container = document.querySelector(".container");
+// share icon
+const shareIcon = document.querySelector(".share-icon");
 
-function popupShow() {
+function popupDesktopShow() {
   popupWindow.classList.toggle("hidden");
+  if (screen.width <= 700) {
+    container.classList.toggle("hidden");
+    shareIcon.classList.toggle("hidden");
+    popupMobileWindow.classList.toggle("hidden");
+  }
 }
 
-if (screen.width <= 700) {
-  console.log("lol");
+function popupMobileShow() {
+  container.classList.toggle("hidden");
+  shareIcon.classList.toggle("hidden");
+  popupMobileWindow.classList.toggle("hidden");
 }
